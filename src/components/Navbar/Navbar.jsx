22678cb8a169e0ac6fb433d37146b2aa3b32c901
Sanrivalin <santiagorivasmolina@gmail.com>
2024-01-22 12:49:55 -0500

    january 22, 2024 commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ export const Navbar = () => {
             <a href="#contact">Contact</a>
           </li>
           <li>
-            <a href="./assets/file/CVSantiagoRivasMolinaJan19_2024.pdf" download="CV_SantiagoRivasMolina.pdf">CV</a>
+            <a href="./assets/CVSantiagoRivasMolinaJan19_2024.pdf" download="CV_SantiagoRivasMolina.pdf">CV</a>
           </li>                   
         </ul>        
       </div>
