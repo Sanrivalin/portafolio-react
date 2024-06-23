2dca27143f4191908a3e170422a4083fc2b69991
Sanrivalin <santiagorivasmolina@gmail.com>
2024-06-23 00:48:10 -0500

    update images

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ export const About = () => {
       <h2 className={styles.title}>{t('aboutTitle')}</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/aboutImage3.png")}
+          src={getImageUrl("hero/sobremi.png")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
